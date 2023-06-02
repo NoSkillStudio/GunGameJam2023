b6e7c74e2278d64b9cf0a9d629843de47185430a
Maksim <sazanovichmaksim@gmail.com>
2023-06-02 23:19:58 +0300

    оны невидимости (в серой не видно белого, новидно синего, в  голубой наоборот )

---
+++ Assets/Scripts/Zones/DisappearanceZone_2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearanceZone_2 : MonoBehaviour
+{
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		
+	}
+}
