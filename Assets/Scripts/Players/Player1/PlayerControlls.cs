4a849ee712e16a9e71b954ece16697c5201a6997
Maksim <sazanovichmaksim@gmail.com>
2023-06-03 10:26:25 +0300

    рефакторинг управления

--- Assets/Scripts/Players/Player1/PlayerControlls.cs
+++ Assets/Scripts/Players/Player1/PlayerControlls.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_1_Controls : MonoBehaviour
+public class PlayerControlls : MonoBehaviour
 {
     public float Speed = 2;
     private Rigidbody2D componentRigidbody;
     private bool isFasingRight = true;
     private Camera camera;
     [SerializeField] private MultipleTargetCamera targetCamera;
+
+    [SerializeField] private KeyCode left;
+    [SerializeField] private KeyCode right;
+    [SerializeField] private KeyCode up;
+    [SerializeField] private KeyCode down;
 
     private void Start()
     {
@@ -18,16 +23,16 @@ public class Player_1_Controls : MonoBehaviour
     private void Update()
     {
         componentRigidbody.velocity = Vector2.zero;
-        if (Input.GetKey(KeyCode.A)) componentRigidbody.velocity += Vector2.left * Speed;
-        if (Input.GetKey(KeyCode.D)) componentRigidbody.velocity += Vector2.right * Speed;
-        if (Input.GetKey(KeyCode.W)) componentRigidbody.velocity += Vector2.up * Speed;
-        if (Input.GetKey(KeyCode.S)) componentRigidbody.velocity += Vector2.down * Speed;
+        if (Input.GetKey(left)) componentRigidbody.velocity += Vector2.left * Speed;
+        if (Input.GetKey(right)) componentRigidbody.velocity += Vector2.right * Speed;
+        if (Input.GetKey(up)) componentRigidbody.velocity += Vector2.up * Speed;
+        if (Input.GetKey(down)) componentRigidbody.velocity += Vector2.down * Speed;
 
-        if (Input.GetKey(KeyCode.D) && !isFasingRight)
+        if (Input.GetKey(right) && !isFasingRight)
         {
             Turn();
         }
-        else if (Input.GetKey(KeyCode.A) && isFasingRight)
+        else if (Input.GetKey(left) && isFasingRight)
         {
             Turn();
         }
